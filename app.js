8b3fb0f81ae569266da293ac9a066915ff71d53f
gmaternos <andersonreiscontato@gmail.com>
2025-05-26 14:05:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// Este arquivo foi criado para resolver o problema de deploy no Render
+// O Render procura por app.js como ponto de entrada, mas o projeto usa server.js
+
+// Simplesmente importa e executa o server.js
+require('./server.js');
